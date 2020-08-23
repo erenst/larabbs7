8fa5adb6bb740bf669a03601d296245b042acf53
erenst <erenst2002@hotmail.com>
2020-08-23 19:14:10 +0930

    生成用户认证代码

--- resources/views/layouts/_header.blade.php
+++ resources/views/layouts/_header.blade.php
@@ -22,10 +22,10 @@
             <ul class="navbar-nav navbar-right">
                 <!-- Authentication Links-->
                 <li class="nav-item">
-                    <a href="#" class="nav-link">登录</a>
+                    <a href="{{route('login')}}" class="nav-link">登录</a>
                 </li>
                 <li class="nav-item">
-                    <a href="#" class="nav-link">注册</a>
+                    <a href="{{route('register')}}" class="nav-link">注册</a>
                 </li>
             </ul>
         </div>
