30096d134b96ee10cec2d677db786e8eb762f96c
erenst <erenst2002@hotmail.com>
2020-08-23 16:37:09 +0930

    基础页面结构

---
+++ resources/views/layouts/_footer.blade.php
@@ -0,0 +1,12 @@
+<footer class="footer">
+    <div class="container">
+        <p class="float-left">
+            Designed and developed by <a href="#" target="_blank">Erenst</a> 
+            <span style="color:#e27575; font-size:14px">❤</span>
+        </p>
+
+        <p class="float-right">
+            <a href="mailto:erenst2002@hotmail.com">Contact Me.</a>
+        </p>
+    </div>
+</footer>
---
+++ resources/views/layouts/_header.blade.php
@@ -0,0 +1,35 @@
+<nav class="navbar navbar-expand-lg navbar-light bg-light navbar-static-top">
+    <div class="container">
+    <!-- Branding Iman=ge-->
+        <a href="{{url('/')}}" class="navbar-brand">
+            LaraBBS7
+        </a>
+
+        <button class="navbar-toggler" type="button" data-toggle="collapse"
+            data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+            aria-expanded="false" aria-label="Toggle navigation"
+        >
+        <span class="navbar-toggler-icon"></span>    
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <!--Left side of navbar -->
+            <ul class="navbar-nav mr-auto">
+        
+            </ul>
+
+            <!--rigt side of navbar -->
+            <ul class="navbar-nav navbar-right">
+                <!-- Authentication Links-->
+                <li class="nav-item">
+                    <a href="#" class="nav-link">登录</a>
+                </li>
+                <li class="nav-item">
+                    <a href="#" class="nav-link">注册</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+
+    
+</nav>
